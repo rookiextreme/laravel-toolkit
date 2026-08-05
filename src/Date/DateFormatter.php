18750cd143b0606561f5b0e9efb2b3f4ab910066
rookiextreme <meor.nazrin1994@gmail.com>
2026-08-06 03:52:41 +0800

    Added test for DateFormatter class functions

--- src/Date/DateFormatter.php
+++ src/Date/DateFormatter.php
@@ -1,15 +1,29 @@
 <?php
-namespace Rookiextreme\Toolkit\Date;
+namespace Rookiextreme\LaravelToolkit\Date;
 
 class DateFormatter
 {
-    public function reverse($date): string
+    public function reverse(string $date): string
     {
+        $timestamp = strtotime($date);
+
+        if($timestamp === false)
+        {
+            throw new \InvalidArgumentException('Invalid date : '.$date);
+        }
+
         return date('Y-m-d', strtotime($date));
     }
 
-    public function regular($date): string
+    public function regular(string $date): string
     {
+        $timestamp = strtotime($date);
+
+        if($timestamp === false)
+        {
+            throw new \InvalidArgumentException('Invalid date : '.$date);
+        }
+
         return date('d-m-Y', strtotime($date));
     }
 }
---
+++ tests/Date/DateFormatterTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Tests\Date;
+
+use PHPUnit\Framework\TestCase;
+use Rookiextreme\LaravelToolkit\Date\DateFormatter;
+
+class DateFormatterTest extends TestCase
+{
+    public function test_reverse_date()
+    {
+        $formatter = new DateFormatter();
+        $this->assertSame(
+            '2026-08-01',
+            $formatter->reverse('01-08-2026'),
+            'Date reverse test successful'
+        );
+    }
+
+    public function test_invalid_reverse_date()
+    {
+        $formmater = new DateFormatter();
+
+        $this->expectException(\Exception::class);
+
+        $formmater->reverse('Hello');
+    }
+
+    public function test_regular_date()
+    {
+        $formatter = new DateFormatter();
+        $this->assertSame(
+            '01-08-2026',
+            $formatter->regular('2026-08-01'),
+        );
+    }
+
+    public function test_invalid_regular_date()
+    {
+        $formmater = new DateFormatter();
+
+        $this->expectException(\Exception::class);
+
+        $formmater->regular('Hello');
+    }
+}
